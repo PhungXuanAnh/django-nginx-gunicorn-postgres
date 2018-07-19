05c7d951194bb8a947b1871c26a3a19f1faeae89
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-07-19 10:58:47 +0700

    add logging configuration

--- django-ckeditor-sample/settings.py
+++ django-ckeditor-sample/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,10 +125,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 ####################################
-    ##  CKEDITOR CONFIGURATION ##
+##  CKEDITOR CONFIGURATION ##
 ####################################
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'MEDIA')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 CKEDITOR_UPLOAD_PATH = 'uploads/ckeditor/'
 
 CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
@@ -209,3 +210,85 @@ CKEDITOR_CONFIGS = {
 #         ]),
 #     }
 # }
+
+
+########################################################################
+#             LOGGING CONFIGURATION
+########################################################################
+LOG_DIR = BASE_DIR + "/logs"
+# SLACK_API_KEY = os.getenv('SLACK_API_KEY', 'None')
+# SLACK_USERNAME = os.getenv('SLACK_USERNAME', "django-nginx-gunicorn-postgres")
+SLACK_API_KEY = json.load(
+    open('/home/xuananh/Dropbox/Work/Other/slack-token-api-key.json',
+         'r'))['phungxuananh']
+SLACK_USERNAME = 'django-dev'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] [%(module)s.%(funcName)s:%(lineno)d] %(levelname)s: %(message)s",
+            'datefmt': "%H:%M:%S",
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        # 'fileHandler': {
+        #     'level': 'INFO',
+        #     'class': 'logging.FileHandler',
+        #     'filename': '/usr/src/app/logs/dashboard.log',
+        #     'formatter': 'verbose'
+        # },
+        'app.ERROR.RotatingFileHandler': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_DIR + '/app.ERROR.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 3,
+            'formatter': 'verbose'
+        },
+        'app.INFO.RotatingFileHandler': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_DIR + '/app.INFO.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 3,
+            'formatter': 'verbose'
+        },
+        'app.DEBUG.RotatingFileHandler': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_DIR + '/app.DEBUG.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 3,
+            'formatter': 'verbose'
+        },
+        'slack.ERROR.SlackerLogHandler': {
+            'level': 'ERROR',
+            'icon_emoji': ':beetle:',
+            'username': SLACK_USERNAME,
+            'api_key': SLACK_API_KEY,
+            'class': 'slacker_log_handler.SlackerLogHandler',
+            'channel': '#django-app'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'app.DEBUG.RotatingFileHandler', 'app.INFO.RotatingFileHandler', 'app.ERROR.RotatingFileHandler'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'slack.logger': {
+            'handlers': ['console', 'slack.ERROR.SlackerLogHandler'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,15 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 from .models import Question
 
+slack_logger = logging.getLogger('slack.logger')
+
+
 def index(request):
+    slack_logger.error('this is poll app in ckeditor sample project')
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
@@ -12,12 +17,15 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
- 
+
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
- 
+
+
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
