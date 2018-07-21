52e8517645e190df05a913c332389de9f4be713c
Phung Xuan Anh <phungxuananh1991@gmail.com>
2018-07-21 21:12:55 +0700

    revert permission add and delete model Question and Choice in admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,22 +3,27 @@ from django.urls import path
 
 from .models import Choice, Question
 
+
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
         # remove add permission
-    def has_add_permission(self, request):
-        return False  
-    
+    #  def has_add_permission(self, request):
+    #     return False
+
     # remove delete permision
-    def has_delete_permission(self, request, obj=None):
-        return False  
+    # def has_delete_permission(self, request, obj=None):
+    #    return False
+    pass
+
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
         # remove add permission
-    def has_add_permission(self, request):
-        return False  
-    
+    # def has_add_permission(self, request):
+    #   return False
+
     # remove delete permision
-    def has_delete_permission(self, request, obj=None):
-        return False  
+
+    # def has_delete_permission(self, request, obj=None):
+    #   return False
+    pass
